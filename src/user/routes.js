892a6bd4c3ac0e436befcd27275fc0357703afac
goleacornelia <goleacornelia725@gmail.com>
2023-10-27 10:00:21 +0100

    cookies

--- src/user/routes.js
+++ src/user/routes.js
@@ -1,13 +1,15 @@
 const { Router } = require("express");
 const userRouter = Router();
 
-const { hashPass, comparePass } = require("../middleware/");
+const { hashPass, comparePass, tokenCheck } = require("../middleware/");
 const { register, login, getAllUsers } = require("./control");
 
 userRouter.post("/", hashPass, register);
 
 userRouter.post("/login", comparePass, login);
 
-userRouter.get("/", getAllUsers);
+userRouter.get("/", tokenCheck, getAllUsers);
+
+userRouter.get("/authCheck", tokenCheck, login);
 
 module.exports = userRouter;
